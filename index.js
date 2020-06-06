38274707d482f01d512aca4d100c306ee8d74fc6
DGomez <ddgomez@uninorte.edu.co>
2020-06-05 20:09:55 -0500

    Con error

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const Sentry = require('@sentry/node');
 
 const api = require('./api');
 const config = require('./config');
 
+Sentry.init({ dsn: 'https://ef7ca8e673a7441193abe4f11d4985c2@o403580.ingest.sentry.io/5266416' });
 const app = express();
 app.use(express.json());
 app.use('/api', api);
